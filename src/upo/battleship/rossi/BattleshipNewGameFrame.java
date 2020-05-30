78684c1d9e0b05fb64c6520bd3f292ac40d2947a
Lorenzo Rossiman <20031485@studenti.uniupo.it>
2020-05-31 01:07:54 +0200

    it finally f***ing works

--- src/upo/battleship/rossi/BattleshipNewGameFrame.java
+++ src/upo/battleship/rossi/BattleshipNewGameFrame.java
@@ -1,14 +1,13 @@
 package upo.battleship.rossi;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
-import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
@@ -17,7 +16,6 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.WindowConstants;
 
-import com.sun.java.swing.plaf.windows.WindowsBorders;
 
 public class BattleshipNewGameFrame extends JFrame implements ActionListener, WindowListener{
 	//attributes
@@ -25,24 +23,37 @@ public class BattleshipNewGameFrame extends JFrame implements ActionListener, Wi
 	private static final int HEIGHT = 200;
 	private static final String TITLE = "New Game";
 	//constructor
-	BattleshipNewGameFrame() {
+	public BattleshipNewGameFrame() {
 		//settings
 		setSize(WIDTH, HEIGHT);
 		setTitle(TITLE);
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		addWindowListener(this);
 		setLayout(new BorderLayout());
+		setBackground(Color.DARK_GRAY);
 		
 		//label per dire che stiamo facendo
-		JLabel newGameLabel = new JLabel("Choose settings:");
+		JLabel newGameLabel = new JLabel("CHOOSE SETTINGS");
 		newGameLabel.setHorizontalAlignment(JLabel.CENTER);
 		
 		//bottoni per confermare e per settare il timer
 		JButton confirmButton = new JButton("CONFIRM");
 		JCheckBox timedCheckBox = new JCheckBox("TIMED");
 		
-		//group of radio buttons
-		//ButtonGroup radioButtonGroup = new ButtonGroup();
+		//radio buttons per scegliere la modalità di gioco
+		JLabel gameModeLabel = new JLabel("Select game mode:");
+		JRadioButton p1vsp2Button = new JRadioButton("P1vsP2");
+		p1vsp2Button.addActionListener(this);
+		JRadioButton p1vsCPUButton = new JRadioButton("P1vsCPU");
+		p1vsCPUButton.addActionListener(this);
+		JPanel gameModePanel = new JPanel();
+		gameModePanel.setLayout(new BorderLayout());
+		gameModePanel.add(gameModeLabel, BorderLayout.NORTH);
+		gameModePanel.add(p1vsp2Button, BorderLayout.CENTER);
+		gameModePanel.add(p1vsCPUButton, BorderLayout.SOUTH);
+		
+		//group of radio buttons for size choice
+		JLabel gridSizeLabel = new JLabel("Select grid size:");
 		JRadioButton sizeSButton = new JRadioButton("5x5");
 		sizeSButton.addActionListener(this);
 		JRadioButton sizeMButton = new JRadioButton("10x10");
@@ -51,20 +62,31 @@ public class BattleshipNewGameFrame extends JFrame implements ActionListener, Wi
 		sizeLButton.addActionListener(this);
 		JRadioButton sizeXLButton = new JRadioButton("20x20");
 		sizeXLButton.addActionListener(this);
-		JPanel radioButtonPanel = new JPanel();
-		radioButtonPanel.setLayout(new FlowLayout());
-		radioButtonPanel.add(sizeSButton);
-		radioButtonPanel.add(sizeMButton);
-		radioButtonPanel.add(sizeLButton);
-		radioButtonPanel.add(sizeXLButton);
+		
+		//JPanel for grouping size radio buttons
+		JPanel radioButtonSizePanel = new JPanel();
+		radioButtonSizePanel.setLayout(new FlowLayout());
+		
+		//TODO: solo uno selezionato alla volta
+		
+		radioButtonSizePanel.add(gridSizeLabel);
+		radioButtonSizePanel.add(sizeSButton);
+		radioButtonSizePanel.add(sizeMButton);
+		radioButtonSizePanel.add(sizeLButton);
+		radioButtonSizePanel.add(sizeXLButton);
 		
 		//panel for all radio buttons
 		JPanel buttonPanel = new JPanel();
-		buttonPanel.add(radioButtonPanel, BorderLayout.BEFORE_FIRST_LINE);
-		buttonPanel.add(timedCheckBox);
 		buttonPanel.setLayout(new FlowLayout());
+		//buttonPanel.setBackground(Color.DARK_GRAY);
+		buttonPanel.add(gameModePanel);
+		buttonPanel.add(radioButtonSizePanel);
+		buttonPanel.add(timedCheckBox);
+		
+		
+		//add everything to frame
+		add(newGameLabel, BorderLayout.BEFORE_FIRST_LINE);
 		add(buttonPanel, BorderLayout.CENTER);
-		add(timedCheckBox, BorderLayout.SOUTH);
 		add(confirmButton, BorderLayout.AFTER_LAST_LINE);
 	}
 	
@@ -72,11 +94,6 @@ public class BattleshipNewGameFrame extends JFrame implements ActionListener, Wi
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
-	}
-	
-	public static void main(String[][] args) {
-		BattleshipNewGameFrame gui = new BattleshipNewGameFrame();
-		gui.setVisible(true);
 	}
 
 	@Override
@@ -119,5 +136,10 @@ public class BattleshipNewGameFrame extends JFrame implements ActionListener, Wi
 	public void windowDeactivated(WindowEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public static void main(String[] args) {
+		BattleshipNewGameFrame gui = new BattleshipNewGameFrame();
+		gui.setVisible(true);
 	}
 }
